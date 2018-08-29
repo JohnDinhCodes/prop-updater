4c4df2cf4c4161cb43e1ca42e6855fe5598fe24a
John Dinh <johnduydinh@gmail.com>
2018-08-28 21:12:44 -0700

    Complete v2

--- app.js
+++ app.js
@@ -1,22 +1,61 @@
 const sqlite3 = require('sqlite3').verbose();
-const fs = require('fs');
 const dirTree = require('directory-tree');
 const PATH = require('path');
 const dignityTree = dirTree('./../Dignity');
 const replace = require('replace-in-file');
+const Promise = require('bluebird');
+const fs = Promise.promisifyAll(require('fs'));
+const moment = require('moment');
+
 
 for (let area of dignityTree.children) {
 
     for (let hospital of area.children) {
-        let db3 = hospital.children[0].children[0].children[0].path.replace(/\\/g, '/');
-        let prop = hospital.children[1].children[0].children[0].path.replace(/\\/g, '/');
-        createData(db3).then(queries => {
-            updateProp(queries, prop)
-        });
+
+        for (let containingFolder of hospital.children) {
+
+            for (let fileFolder of containingFolder.children) {
+
+                let prop = '';
+                let db3 = '';
+
+                if (fileFolder.name.includes('zconfig')) {
+                    prop = fileFolder.children[0].path;
+                    if (currentDB3Tree(prop.replace(/zDI[\S]+/, getContainingDB3Folder(prop)))) {
+                        currentDB3Tree(prop.replace(/zDI[\S]+/, getContainingDB3Folder(prop))).children.forEach(file => {
+                            if (file.name.includes(getDB3FileName(prop))) {
+                                db3 = file.path;
+                            }
+                        });
+                    } else {
+                        console.log(`No DB3 File found at: ${currentDB3Tree(prop.replace(/zDI[\S]+/, '')).path}`);
+                    }
+                }
+                if (prop.length > 0 && db3.length > 0) {
+                    createData(db3).then(queries => {
+                        updateProp(queries, prop);
+                    });
+                }
+            }
+        }
     }
 }
 
+// Functions related to DB3
+function getContainingDB3Folder(prop) {
+    return fs.readFileSync(prop, 'utf8').match(/zCA[\w | .]+[/ex_inp]+/)[0].replace('*', '');
+}
 
+function currentDB3Tree(path) {
+    return dirTree(path)
+}
+
+function getDB3FileName(prop) {
+    return fs.readFileSync(prop, 'utf8').match(/dign[\S]+835[\w]+/)[0];
+}
+
+
+// Function with Promise that returns queries as an object
 function createData(database) {
     let sql = `SELECT CLP01 
     FROM L2100
@@ -29,7 +68,6 @@ function createData(database) {
             if (err) {
                 return console.error(err.message);
             }
-            console.log('Connected to the database');
         });
         const queries = [];
         db.each(sql, (err, row) => {
@@ -48,42 +86,45 @@ function createData(database) {
     });
 }
 
-let data = [];
-createData('../Dignity/Arizona/hospital1/zCA_v2.15_Y18_w31/input/testing2.db3').then(value => {
-    for (let claim of value) {
-        data.push(claim.clp01);
-    }
-});
-
 function updateProp(queries, prop) {
+    // Make Delimited String With Search Results From DB3
     const data = [];
-    let string = 'script.ignoreClaims=';
-    
+    let claimString = 'script.ignoreClaims=';
+
     for (let claim of queries) {
         data.push(claim.clp01);
     }
 
     for (let i = 0; i < data.length; i++) {
         if (i === data.length - 1) {
-            string += data[i];
+            claimString += data[i];
         } else {
-            string += (data[i] + '|');
+            claimString += (data[i] + '|');
         }
     }
-    
+
+    // Make String With Current Week Number
+    let weekNumber = moment().date(0).format('WW');
+    let weekString = `${weekNumber} with Week Number`
+
+    //Make String With Current Year
+    let yearNumber = moment().date(0).format('YY');
+    let yearString = `Y${yearNumber} with Year`
+
     const options = {
         files: prop,
-        from: /^script.ignoreClaims=?[^\s]+/m,
-        to: string,
+        from: [/^script.ignoreClaims=?[^\s]+/m, /[0-9]+ with Week Number/, /Y[0-9]+ with Year/],
+        to: [claimString, weekString, yearString]
     };
-    
-    
-    replace(options)
-    .then(changes => {
-        console.log('Modified files:', changes.join(', '));
-    })
-    .catch(error => {
-        console.error('Error occured:', error)
-    });
 
+
+    replace(options)
+        .then(changes => {
+            if (changes.length > 0) {
+                console.log('Modified files:', changes.join(', '));
+            }
+        })
+        .catch(error => {
+            console.error('Error occured:', error)
+        });
 }
